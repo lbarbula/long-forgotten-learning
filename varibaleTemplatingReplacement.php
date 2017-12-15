6b498d707dba627b5908ddd098303a530f2b91ed
Lucas Barbula <lucas.barbula@gmail.com>
2017-12-14 20:51:31 -0700

    string and things

--- varibaleTemplatingReplacement.php
+++ varibaleTemplatingReplacement.php
@@ -5,13 +5,27 @@ $currencyDeclaration = "My $currency is worth less than the dollar.";
 // echo $currencyDeclaration;
 
 $templateInsertion = "baby back";
-$stringToInsertInto = "I want my {$templateInsertion} {$templateInsertion} {$templateInsertion}"."ed ribs.";
-// echo $stringToInsertInto;
+$ribs = "ribs";
+$stringToInsertInto = "I want my {$templateInsertion} {$templateInsertion} {$templateInsertion}"."ed "."{$ribs}";
 
-// echo <<<EOT
-//       A big block of
-//     text though i'm not
-//     sure why.
-// EOT;
+function changeMyText($stringToFind, $stringToChange, $whatToReplaceWith) {
+  if(strpos($stringToFind, $stringToChange) >= 0) {
+    return str_replace($stringToFind, $whatToReplaceWith, $stringToChange);
+  } else {
+    return "Can't find that string to replace.";
+  }
+}
 
-print "text";
+function manipulateSomeText($stringToSub, $whenToStart, $howManyCharactersToAccept) {
+  return substr($stringToSub, $whenToStart, $howManyCharactersToAccept);
+}
+
+echo changeMyText($ribs, $stringToInsertInto, $currency);
+echo "\n";
+echo manipulateSomeText($stringToInsertInto, 4, 5);
+echo "\n";
+echo manipulateSomeText($stringToInsertInto, 0, -5);
+echo "\n";
+echo manipulateSomeText(changeMyText($ribs, $stringToInsertInto, $currency), 0, 0);
+$arrayVar = str_split($stringToInsertInto, 3);
+print_r($arrayVar);
